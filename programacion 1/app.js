345c022e3aab5bd110e458db1f013502ac4edfab
ailen2721 <152309182+ailen2721@users.noreply.github.com>
2023-11-28 13:41:21 -0300

    subiendo los trabajos

---
+++ programacion 1/app.js
@@ -0,0 +1,68 @@
+//Clase 1
+//este comentario de una linea 
+/*comentario
+multilinea  
+*/
+
+// las variables so una caja que almanecenan un valor
+ver miNombre; 
+
+//Almanecenar un valor en la variable creada 
+miNombre = "Ailen";
+
+//Ejercicio 1: 
+//crear variable edad y guardarle un numero 
+
+var edad; //creamos la variable 
+edad = 22 // la inicilizamos 
+console.log(edad); // esto muestra en la consola 
+
+//Ejercicio 2:
+// inicializar tres variables a,b y c con los valores 
+// 5, 10 y " I am a"
+// // cambia solo el codigo debajo de la linea 
+var a = 5; 
+var b = 10;
+var c = "I am a";
+// // cambia solo el codigo encima de esta linea 
+
+
+a = a + 1; 
+b = b + 5; 
+c = c + " string!"; 
+console.log(a);
+console.log(b);
+console.log(c);
+//clase 2 
+var nombre = prompt(´ingrese su nombre: ´)
+console.log(´hola´ , nombre ); 
+
+
+//clase 3
+// ejercicio 3 
+// calcular el aerea y el perimetro de un cuadrado 
+let lado = 40; 
+let area = lado* lado; 
+let perimetro = lado * 4; 
+console.log(´el area de un cuadrado es: ´ , area ); 
+console.log(´el perimetro de un cuadrado es: ´ , perimetro );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
